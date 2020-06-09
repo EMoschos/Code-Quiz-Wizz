7f48e98d9be26279da17bea5f29e8a0f5c235822
Eleftherios Moschos <tmoschos@bigpond.com>
2020-06-09 23:09:55 +0930

    Included function to control when timer starts and stops

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 // Script page
 
 var timerDisplay = document.querySelector("#time");
+var startButton = document.querySelector("#startQuiz");
+var question1 = document.querySelector("#quest1")
+var question2 = document.querySelector("#quest2")
+var startMain = document.querySelector("#startMain")
+var q1Button = document.querySelector("#quest2btn")
 
-var timer = 5;
-
-startTimer();
+var timer = 60;
+var interval;
+timerDisplay.textContent = timer;
 
 function startTimer() {
-    var interval = setInterval(function () {
+    questOne();
+    interval = setInterval(function () {
         if (timer > 0) {
             timer--;
             timerDisplay.textContent = timer;
@@ -18,3 +24,18 @@ function startTimer() {
         }
     }, 1000)
 };
+
+function questOne() {
+question1.style.display = "block";
+startMain.style.display = "none";
+}
+
+function questtwo() {
+    question1.style.display = "none";
+    question2.style.display = "block";
+    clearInterval(interval);
+    }
+
+startButton.addEventListener("click", startTimer);
+//startButton.addEventListener("click", questOne);
+q1Button.addEventListener("click", questtwo);
