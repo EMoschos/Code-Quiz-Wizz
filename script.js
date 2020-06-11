8731e0c8ff2849a59a90a286c24ff99ee229eb0c
Eleftherios Moschos <tmoschos@bigpond.com>
2020-06-11 23:33:37 +0930

    Added function to save score

--- script.js
+++ script.js
@@ -2,18 +2,21 @@
 
 var timerDisplay = document.querySelector("#time");
 var startButton = document.querySelector("#startQuiz");
-var question1 = document.querySelector("#quest1")
-var startMain = document.querySelector("#startMain")
-var scoreForm = document.querySelector("#scoreForm")
-var score = document.querySelector("#score")
-var textAnswer = document.querySelector("#textAnswer")
+var question1 = document.querySelector("#quest1");
+var startMain = document.querySelector("#startMain");
+var scoreForm = document.querySelector("#scoreForm");
+var score = document.querySelector("#score");
+var textAnswer = document.querySelector("#textAnswer");
+var saveScore = document.querySelector("#saveScore");
+var listScore = document.querySelector("#listScore");
+var inputName = document.querySelector("inlineFormInputName")
 
 // Div Elements that Quiz details will show the content
-var qPlace = document.querySelector("#qPlace")
-var ansOne = document.querySelector("#ansOne")
-var ansTwo = document.querySelector("#anstwo")
-var ansThree = document.querySelector("#ansThree")
-var ansFour = document.querySelector("#ansFour")
+var qPlace = document.querySelector("#qPlace");
+var ansOne = document.querySelector("#ansOne");
+var ansTwo = document.querySelector("#anstwo");
+var ansThree = document.querySelector("#ansThree");
+var ansFour = document.querySelector("#ansFour");
 
 
 var timer = 60;
@@ -32,7 +35,7 @@ var quizSet = [{
 },
 {
     Quest: "Q3: What is JavaScript used for?",
-    ansSet: ["Building static websites", "Building websites in Indonesia only", "Building websites for that only show text", "Building dynamic websites"],
+    ansSet: ["Building static websites", "Building websites in Indonesia only", "Building websites that only show text", "Building dynamic websites"],
     correctAns: "Building dynamic websites"
 },
 {
@@ -41,8 +44,10 @@ var quizSet = [{
     correctAns: "Variable"
 }];
 
+var highScores = "";
 
-var i = 0
+var i = 0;
+
 function setQuestArray(i) {
     qPlace.textContent = quizSet[i].Quest;
     ansOne.textContent = quizSet[i].ansSet[0];
@@ -52,7 +57,7 @@ function setQuestArray(i) {
 };
 
 function startTimer() {
-    questOne()
+    questOne();
     setQuestArray(i);
     interval = setInterval(function () {
         if (timer > 0) {
@@ -77,7 +82,7 @@ function responseClick(event) {
         }
         else {
             clearInterval(interval);
-            setTimeout(function() { showForm(); }, 2000);
+            setTimeout(function () { showForm(); }, 2000);
             score.textContent = (" " + timer);
             timerDisplay.textContent = timer;
         }
@@ -85,28 +90,27 @@ function responseClick(event) {
 }
 
 function checkAnswer() {
-    if (i === 0 && event.target === ansTwo) {
+    if ((i === 0 && event.target === ansTwo) || (i === 1 && event.target === ansThree) || (i === 2 && event.target === ansFour) || (i === 3 && event.target === ansOne)) {
         textAnswer.textContent = "Last Answer was CORRECT!!!";
         textAnswer.style.backgroundColor = "Green";
     }
-    else if (i === 1 && event.target === ansThree) {
-        textAnswer.textContent = "Last Answer was CORRECT!!!";
-        textAnswer.style.backgroundColor = "Green";
-    }
-    else if (i === 2 && event.target === ansFour) {
-        textAnswer.textContent = "Last Answer was CORRECT!!!";
-        textAnswer.style.backgroundColor = "Green";
-    }
-    else if (i === 3 && event.target === ansOne) {
-        textAnswer.textContent = "Last Answer was CORRECT!!!";
-        textAnswer.style.backgroundColor = "Green";
-    }
+    // else if (i === 1 && event.target === ansThree) {
+    //     textAnswer.textContent = "Last Answer was CORRECT!!!";
+    //     textAnswer.style.backgroundColor = "Green";
+    // }
+    // else if (i === 2 && event.target === ansFour) {
+    //     textAnswer.textContent = "Last Answer was CORRECT!!!";
+    //     textAnswer.style.backgroundColor = "Green";
+    // }
+    // else if (i === 3 && event.target === ansOne) {
+    //     textAnswer.textContent = "Last Answer was CORRECT!!!";
+    //     textAnswer.style.backgroundColor = "Green";
+    // }
     else {
-        textAnswer.textContent = "Oh no last answer was WRONG!!!";
+        textAnswer.textContent = "Oh no last answer was WRONG!!! (-10 seconds)";
         textAnswer.style.backgroundColor = "red";
         timer = timer - 10;
     }
-
 }
 
 function questOne() {
@@ -120,5 +124,44 @@ function showForm() {
     clearInterval(interval);
 }
 
+function logScore() {
+    event.preventDefault();
+    var x = document.getElementById("inlineFormInputName").value;
+    var scoresText = x + " Your Score is: " + timer;
+    console.log(scoresText)
+
+    //var x = highScores;
+    
+
+
+    // Return from function early if submitted todoText is blank
+    if (x === "") {
+      alert("You need to enter your initals")
+      return;
+    }
+    else if (score < 1) {
+        alert("Your score is ZERO and can't be logged... Try harder!!!")
+        return;
+    } 
+    var li = document.createElement("li");
+    li.setAttribute("class", "list-group-item list-group-item-action list-group-item-dark");
+    li.textContent = scoresText;
+    listScore.appendChild(li);
+    //highScores.push(scoresText);
+    //renderScores()
+    //inputName.value = "";
+}
+
+// function renderScores(){
+//     for (var s = 0; s < highScores.length; s++) {
+//         var x = highScores;
+    
+//         var li = document.createElement("li");
+//         li.textContent = x;
+//         listScore.appendChild(li);
+//       }
+// }
+
 startButton.addEventListener("click", startTimer);
 question1.addEventListener("click", responseClick);
+saveScore.addEventListener("click", logScore)
